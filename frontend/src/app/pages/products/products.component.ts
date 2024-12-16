ab9f6257ae3d7f7b8c2f7213435d375e1ffd2b34
esat avci <esat.avci@dataholix.de>
2024-12-16 22:30:00 +0100

    add new page products

--- frontend/src/app/pages/products/products.component.ts
+++ frontend/src/app/pages/products/products.component.ts
@@ -1,12 +1,60 @@
 import { Component } from '@angular/core';
+import { NavComponent } from '../../components/nav/nav.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [
+    NavComponent,
+    MatIconModule,
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonToggleModule,
+  ],
   templateUrl: './products.component.html',
-  styleUrl: './products.component.scss'
+  styleUrl: './products.component.scss',
 })
 export class ProductsComponent {
-
+  openAddProductDialog() {
+    throw new Error('Method not implemented.');
+  }
+  editProduct(_t25: {
+    name: string;
+    description: string;
+    price: number;
+    imagePath: string;
+  }) {
+    throw new Error('Method not implemented.');
+  }
+  removeItem(index: number) {
+    throw new Error('Method not implemented.');
+  }
+  products = [
+    {
+      name: 'Product 1',
+      description: 'Description 1',
+      price: 10.99,
+      imagePath: 'https://via.placeholder.com/150',
+    },
+    {
+      name: 'Product 2',
+      description: 'Description 2',
+      price: 15.49,
+      imagePath: 'https://via.placeholder.com/150',
+    },
+    {
+      name: 'Product 3',
+      description: 'Description 3',
+      price: 30,
+      imagePath: 'https://via.placeholder.com/150',
+    },
+  ];
 }
