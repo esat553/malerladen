d07cc9605554a538a259de92a75fb6eaf6a019cb
esat avci <esat.avci@dataholix.de>
2024-12-16 22:28:52 +0100

    erstelle eine Navigation-Bar

---
+++ frontend/src/app/components/nav/nav.component.ts
@@ -0,0 +1,19 @@
+import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { AuthService } from '../../services/authService/auth-service.service';
+
+@Component({
+  selector: 'app-nav',
+  standalone: true,
+  imports: [MatButtonModule, MatIconModule],
+  templateUrl: './nav.component.html',
+  styleUrl: './nav.component.scss',
+})
+export class NavComponent {
+  private authService = inject(AuthService);
+
+  logout() {
+    this.authService.logout();
+  }
+}
